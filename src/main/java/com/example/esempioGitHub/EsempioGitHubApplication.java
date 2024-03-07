f8553e2d85a7eeb4c360e7484fd60cf619c1ab04
AndreaDb03 <andreadebenedetto2003@gmail.com>
2024-03-07 17:24:39 +0100

    Initial commit

---
+++ src/main/java/com/example/esempioGitHub/EsempioGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.esempioGitHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsempioGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsempioGitHubApplication.class, args);
+	}
+
+}
